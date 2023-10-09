e269c9cc9196be35a96c8028ccc61dbcaa3a3204
IliaU <ilia82@mail.ru>
2023-10-09 23:32:31 +0300

    Начал делать параметры и понял что перебор. Плагин тут надо убрать так как между классами как передавать не понятно да и в базу придётся делать кучу запросов что с точки зрения производительности тоже плохо

--- Common/ParamList.cs
+++ Common/ParamList.cs
@@ -13,5 +13,24 @@ namespace Common
     /// </summary>
     public sealed class ParamList : ParamBase.ParamListBase
     {
+        /// <summary>
+        /// Идентификатор параметров для которых предназначено задание и объекта с которым эти параметры связаны
+        /// </summary>
+        public Guid ObjParamListId;
+
+        /// <summary>
+        /// Имя параметра
+        /// </summary>
+        public string ParamName;
+
+        /// <summary>
+        /// Значение параметра которое может быть кастомным созданное в плагине пользователя
+        /// </summary>
+        public Param ParamValue;
+
+        /// <summary>
+        /// Параметр может содержать вложения
+        /// </summary>
+        public ParamList ParamListChildrens;
     }
 }
